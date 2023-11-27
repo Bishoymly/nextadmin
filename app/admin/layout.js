a699a2f063d2ee77d466ff4606838d74717ee1e6
Bishoy Labib <Bishoy.Labib@integrant.com>
2023-11-27 11:15:14 -0800

    minor styling

--- app/admin/layout.js
+++ app/admin/layout.js
@@ -16,7 +16,7 @@ export default async function AdminLayout({ children }) {
 
   return (
     <div className="flex flex-col min-h-screen min-w-full bg-background max-h-screen">
-      <nav className="flex justify-between items-center border-b border-border px-4 py-2">
+      <nav className="flex-none flex justify-between items-center border-b border-border px-4 py-2">
         <Link href="/admin" className="flex flex-row items-center">
           <Image src="/NextAdminIcon.png" width={26} height={26} />
           <span className="text-lg font-bold ml-2">Next Admin</span>
@@ -25,7 +25,7 @@ export default async function AdminLayout({ children }) {
           <ModeToggle />
         </div>
       </nav>
-      <div className="flex flex-col lg:flex-row lg:space-x-12 lg:space-y-0">
+      <div className="flex-1 flex flex-col lg:flex-row lg:space-x-12 lg:space-y-0">
         <aside className="p-2 border-b lg:w-1/5 lg:border-r lg:border-b-0">
           <SideNav items={items} />
         </aside>
